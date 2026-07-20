cf78c0d8782a3982fe7467fd6ed97027b304f230
SujanNaik890 <Sujannishantgajanan68@gmail.com>
2026-07-21 00:06:09 +0530

    Updated LinkedList implementation

--- LinkedList/DoublyLinkedList/DlistImplimentation.java
+++ LinkedList/DoublyLinkedList/DlistImplimentation.java
@@ -1,2 +1,113 @@
 public class DlistImplimentation {
+    Node head=null;
+    public class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    void insertAtFirst(int data){
+        Node node =new Node(data);
+        if(head==null){
+            head=node;
+            return;
+        }
+        node.next=head;
+        head.prev=node;
+        head=node;
+    }
+    void insertAtEnd(int data){
+        Node node=new Node(data);
+        if(head==null){
+            head=node;
+            return;
+        }Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        node.prev=temp;
+        temp.next=node;
+
+    }
+    void insertAtposition(int data,int pos){
+        if(head==null){
+            return;
+        }
+        Node node=new Node(data);
+        if(pos==1){
+            node.next=head;
+            head.prev=node;
+            head=node;
+            return;
+        }
+        Node temp=head;
+        int i=0;
+        while(i<pos-2 && temp.next!=null){
+            temp=temp.next;i++;
+        }
+        node.next=temp.next;
+        temp.next=node;
+        temp.next.prev=node;
+        node.prev=temp;
+    }
+     void deleteAtFrist(){
+        if(head==null){
+            return;
+        }
+        head.next.prev=null;
+        head=head.next;
+
+     }
+     void deleteAtEnd(){
+        if(head==null)return;
+        if(head.next==null)head=null;
+        Node temp=head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next.prev=null;
+        temp.next=null;
+     }
+     void deleteAtPos(int data){
+        if(head==null)return;
+        Node temp=head;
+        while(temp.next.next!=null && temp.next.data!=data){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+        temp.next.prev=temp;
+     }
+    void display(){
+        if(head == null)
+                return;
+        Node temp=head;
+        while(temp.next!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println(temp.data);
+        while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.prev;
+            }
+            System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DlistImplimentation dlist=new DlistImplimentation();
+        dlist.insertAtEnd(200);
+        dlist.insertAtFirst(10);
+        dlist.insertAtFirst(20);
+        dlist.insertAtFirst(30);dlist.insertAtEnd(300);
+        dlist.insertAtposition(100,3);
+        dlist.display();
+        dlist.deleteAtFrist();
+        dlist.deleteAtEnd();
+        dlist.deleteAtPos(100);
+        dlist.display();
+    }
 }
--- LinkedList/singlyLinkedlist/SinglelistImplementation.java
+++ LinkedList/singlyLinkedlist/SinglelistImplementation.java
@@ -1,2 +1,107 @@
 public class SinglelistImplementation {
+    public static void main(String[] args) {
+  LinkeedList list=new LinkeedList();
+  list.insertAtFirst(10);
+  list.insertAtFirst(20);
+
+  list.insertAtFirst(30);list.insertAtEnd(100);
+  list.insertAtposition(80,6);
+  list.Search(100);
+  list.display();
+  list.deleteAtEnd();
+  list.deleteAtFirst();
+  list.deleteAtPosition(10);
+  list.display();
+    }
+
 }
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+    }
+}
+class LinkeedList{
+    Node head;
+    void insertAtFirst(int data){
+        Node node=new Node(data);
+        node.next=head;
+        head=node;
+    }
+    void insertAtEnd(int data){
+        if(head==null){
+//            insertAtFirst(data);
+        }
+        Node node =new Node(data);
+        Node temp= head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=node;
+    }
+    void insertAtposition(int data,int pos) {
+        if (head == null) {
+            insertAtFirst(data);
+            return;
+        }
+        if (pos == 1) {
+            insertAtFirst(data);
+            return;
+        }
+        Node node = new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i<pos-1 && temp.next!=null){
+            temp=temp.next;i++;
+        }
+        node.next=temp.next;
+        temp.next=node;
+
+    }
+    void deleteAtFirst(){
+        if(head==null)return;
+        Node temp=head;
+        head=head.next;
+        temp=null;
+    }
+    void deleteAtEnd(){
+        if(head==null)return;
+        if(head.next==null) {
+            head=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next.next!=null){temp=temp.next;}
+        temp.next=null;
+
+    }
+    void deleteAtPosition(int data){
+        if(head==null)return;
+        Node temp=head;
+        while(temp.next.next!=null &&temp.next.data!=data){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+
+    }
+    void Search(int data){
+        if(head==null)return;
+        Node temp=head;
+        int count=1;
+        while(temp.data!=data &&temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        System.out.println("Element " +data+" Present At index "+count);
+    }
+    void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
