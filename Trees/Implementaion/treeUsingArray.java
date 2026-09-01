04430a049c45b8eca9f8b9f8380ffb5c899bb181
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-09-01 23:00:06 +0530

    Tree implementation

---
+++ Trees/Implementaion/treeUsingArray.java
@@ -0,0 +1,17 @@
+public class treeUsingArray {
+    class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    Node ;
+    void insert(int data){
+        if()
+    }
+}
